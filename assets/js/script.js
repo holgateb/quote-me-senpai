679488ef74e885c39e257fb4f901f18b4aa032a1
Robert Bishop <rbishop85@gmail.com>
2022-07-12 22:27:35 -0700

    printed quote and answers to page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,31 @@
 
 
-var test = [
-    {"anime":"Gintama","character":"Gintoki Sakata","quote":"It's often said, \"People who are similar can be called friends\", right? You haven't been making a good life for yourself, have you? Well, I guess I'm no better.. People don't try to make a life that they can't be proud of... They have the intention of staying on the straight path, but out of blue, they're in the dirt. But, even so, with heart and soul you try to brake through. There will be a day, that even dirt will dry and fall off.\n\n(Gintoki to Catherine\n\"Gintama chapter 24, page 19\")"},
-    {"anime":"Shinsekai Yori","character":"Shinsekai Yori","quote":"The power of imagination is what changes everything."},
-    {"anime":"Death Note","character":"Ryuk","quote":"Humans are so... interesting."},
-    {"anime":"Denpa Kyoushi","character":"Shikishima Kiriko","quote":"Getting kicked out of school won't kill me, but losing my way of life most certainly will."},
-    {"anime":"Howl's Moving Castle","character":"Howl","quote":"Sorry, I've had enough of running away, Sophie. Now I've got something I want to protect. It's you."},
-    {"anime":"Naruto","character":"Kiba Inuzuka","quote":"It's not bad enough they buried us alive in the oversized dung heap, but now they're stealing our chakra. You gotta give these guys credit for thoroughness!"},
-    {"anime":"Umineko no Naku Koro ni","character":"Ushiromiya George","quote":"You need three types of power to control the world. One is influence. One is wealth. The third is... force."},
-    {"anime":"Solanin","character":"Kato Kenichi","quote":"When you lose something, you gotta find something to take its place. I guess it's just time... for me to move forward for once."},
-    {"anime":"Naruto","character":"Kisame Hoshigaki","quote":"[after learning Guy Sensei's name. Might Guy] Yeah, mighty stupid looking guy!"},
-    {"anime":"Gakuen Alice","character":"Mikan Sakura","quote":"To me, death is not the scariest thing at all."}
-];
+// var test = [
+//     {"anime":"Gintama","character":"Gintoki Sakata","quote":"It's often said, \"People who are similar can be called friends\", right? You haven't been making a good life for yourself, have you? Well, I guess I'm no better.. People don't try to make a life that they can't be proud of... They have the intention of staying on the straight path, but out of blue, they're in the dirt. But, even so, with heart and soul you try to brake through. There will be a day, that even dirt will dry and fall off.\n\n(Gintoki to Catherine\n\"Gintama chapter 24, page 19\")"},
+//     {"anime":"Shinsekai Yori","character":"Shinsekai Yori","quote":"The power of imagination is what changes everything."},
+//     {"anime":"Death Note","character":"Ryuk","quote":"Humans are so... interesting."},
+//     {"anime":"Denpa Kyoushi","character":"Shikishima Kiriko","quote":"Getting kicked out of school won't kill me, but losing my way of life most certainly will."},
+//     {"anime":"Howl's Moving Castle","character":"Howl","quote":"Sorry, I've had enough of running away, Sophie. Now I've got something I want to protect. It's you."},
+//     {"anime":"Naruto","character":"Kiba Inuzuka","quote":"It's not bad enough they buried us alive in the oversized dung heap, but now they're stealing our chakra. You gotta give these guys credit for thoroughness!"},
+//     {"anime":"Umineko no Naku Koro ni","character":"Ushiromiya George","quote":"You need three types of power to control the world. One is influence. One is wealth. The third is... force."},
+//     {"anime":"Solanin","character":"Kato Kenichi","quote":"When you lose something, you gotta find something to take its place. I guess it's just time... for me to move forward for once."},
+//     {"anime":"Naruto","character":"Kisame Hoshigaki","quote":"[after learning Guy Sensei's name. Might Guy] Yeah, mighty stupid looking guy!"},
+//     {"anime":"Gakuen Alice","character":"Mikan Sakura","quote":"To me, death is not the scariest thing at all."}
+// ];
 
 pullRandomQuotes();
 
 function pullRandomQuotes() {
 
-    // fetch(`https://animechan.vercel.app/api/quotes`)
-    // .then(function (response) {
-    //     return response.json();
-    // })
-    // .then(function (data) {
-    //     processQuizItems(data);
-    // })
+    fetch(`https://animechan.vercel.app/api/quotes`)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        processQuizItems(data);
+    })
 
-    processQuizItems(test);
+    // processQuizItems(test);
 }
 
 function processQuizItems(data) {
@@ -48,20 +48,24 @@ function processQuizItems(data) {
         text: (data[indexOfObject].character + " from: " + data[indexOfObject].anime),
         answer: true
         };
+    var correctAnswer = data[indexOfObject].character
+    console.log(correctAnswer);
     // Remove the selected quote object from the overall array of potential quotes
     data.splice(indexOfObject, 1);
     // Push first answer object to list of answers
     answerList.push(answerItem);
-    // Loop over remaining quotes data creating false answers, deleting answers that excede total list length of 4 answers
+    // Loop over remaining quotes data creating false answers, skipping answers already in the list and deleting answers that exceed total list length of 4 answers
     data.forEach(function(data) {
-        answerItem = {
-            text: (data.character + " from: " + data.anime),
-            answer: false
-        };
-        answerList.push(answerItem);
-        if (answerList.length > 4) {
-            answerList.length = 4;
-        }
+        if(!answerList.some(e => e.text === (data.character + " from: " + data.anime))) {
+            answerItem = {
+                text: (data.character + " from: " + data.anime),
+                answer: false
+            };
+            answerList.push(answerItem);
+            if (answerList.length > 4) {
+                answerList.length = 4;
+            }
+        }   
     });
     // Shuffle answer list
     var i = answerList.length, k , temp;      // k is to generate random index and temp is to swap the values
@@ -76,9 +80,16 @@ function processQuizItems(data) {
 }
 
 function printQuiz(quote, answers) {
+    var quoteEl = document.getElementById("quote");
+    var answersEl = document.getElementById("answers");
     console.log("Chosen quote:")
     console.log(quote);
     console.log("Randomized Answers List:")
     console.log(answers);
-
+    quoteEl.innerHTML = "";
+    quoteEl.innerHTML = `<h2 class="center-align">${quote}</h2>`
+    answersEl.innerHTML = "";
+    answers.forEach(function(data) {
+        answersEl.innerHTML += `<button style="text-transform:none" class="btn" data-answer="${data.answer}">${data.text}</button>` 
+    });
 };
