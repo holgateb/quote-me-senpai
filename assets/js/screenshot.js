419342c17b9346f5b2749b5b3be88de835f7f4ac
Robert Bishop <rbishop85@gmail.com>
2022-07-19 19:33:42 -0700

    removed screenshot button from main quiz page

--- assets/js/screenshot.js
+++
@@ -1,7 +0,0 @@
-function screenshot() {
-    html2canvas(document.querySelector("#main")).then(canvas => {
-    $('#blank').attr('href', canvas.toDataURL("image/jpg"));
-    $('#blank').attr("download", "quote.jpg");
-    $('#blank')[0].click();
-    });
-};
