6772e47683143b928b9ce48456f20944d67fb646
Robert Bishop <rbishop85@gmail.com>
2022-07-19 19:14:30 -0700

    small code tweaks on random quote page, and fixed dropdown menu functionality

--- assets/js/randomquote generator.js
+++ assets/js/randomquote generator.js
@@ -1,13 +1,12 @@
 var questionTag = document.getElementById("question");
 var displayQuestionEl = document.getElementById("questionpanel");
-// var correctAnswerEl = document.getElementById("btn-2");
-// var answer1OptionEl =document.getElementById("btn-1");
-// var answer3OptionEl =document.getElementById("btn-3");
-// var answer4OptionEl =document.getElementById("btn-4");
 var animedisplayEl = document.getElementById("anime");
 var characterdisplayEl = document.getElementById("character")
 
- pullRandomQuotes();
+pullRandomQuotes();
+
+//   Event trigger for dropdown menu
+$('.dropdown-trigger').dropdown();
 
 function pullRandomQuotes() {
 
@@ -25,11 +24,6 @@ function pullRandomQuotes() {
 function processQuizItems(data) {
     console.log(data);
 
-    var i =0;
-    //i = i+1;
-   // displayQuestionEl.innerHTML = data.quote;
-    //console.log(data.quote.length);
-
     if (data.quote.length >= 145){
         //console.log("hello");
         pullRandomQuotes();  
@@ -42,22 +36,4 @@ function processQuizItems(data) {
         //console.log(data.quote);    
     }
 
-}
-
-var animeName = ['Inuyasha','FaiFlowright','Satashi','AmuroRay','Momoko','Chibiusa','Masahiro','Rogue','Sora','Kouta','RoyRiza','KingBradley','Ally','TōtōsaiKaede']
-                
-function shuffle(array) {
-                array.sort(()=>Math.random()-.5);
-                   }
-
-function answerQuestion(data){
-    console.log(data);
-    correctAnswerEl.innerHTML = data.character;
-   for (var i =0 ; i <= 4 ; i++){
-    shuffle(animeName);
-    answer1OptionEl.innerHTML = animeName[i];
-    answer3OptionEl.innerHTML = animeName[i+1];
-     answer4OptionEl.innerHTML = animeName[i+3];
-
-   }
-}
+}                
